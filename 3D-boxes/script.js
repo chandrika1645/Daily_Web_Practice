b3eb81eb74d5874eb6644d7f8ef4e5abc9b4b9b8
Chandrika Sharma <111525029+chandrika1645@users.noreply.github.com>
2023-05-22 23:39:02 +0530

    Update script.js

--- 3D-boxes/script.js
+++ 3D-boxes/script.js
@@ -1,9 +1,9 @@
 const boxesContainer = document.getElementById('boxes')
 const btn = document.getElementById('btn')
 
-btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))  //Toggling the 'big' class will trigger the CSS transitions
 
-function createBoxes() {
+function createBoxes() {                     //create a grid of boxes
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const box = document.createElement('div')
