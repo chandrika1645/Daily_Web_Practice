58aee12acf7fad449863457b5f679fab4c4af93d
Chandrika Sharma <111525029+chandrika1645@users.noreply.github.com>
2023-06-10 05:22:21 +0530

    Update script.js

--- Count_down/script.js
+++ Count_down/script.js
@@ -1,6 +1,6 @@
 const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
-const finalMessage = document.querySelector('.final')
+const finalMessage = document.querySelector('.final') 
 const replay = document.querySelector('#replay')
   
 runAnimation()  
